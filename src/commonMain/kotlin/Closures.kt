7e2637e006a8a13b347e92a3f35a37e73a6a0d5f
Hall of Famer <worldoffame@hotmail.com>
2021-08-24 23:35:52 -0400

    Update Test for Closures.kt

--- src/commonMain/kotlin/Closures.kt
+++ src/commonMain/kotlin/Closures.kt
@@ -2,6 +2,7 @@ package com.mysidia.kttalk
 
 typealias Predicate = () -> Boolean
 typealias Block = () -> Unit
+typealias Closure = () -> Any?
 
 infix fun Predicate.whileTrue(code: Block) {
     if(this()){
@@ -26,12 +27,32 @@ inline infix fun Block.ensure(code: Block){
     }
 }
 
-inline fun Block.ifCurtailed(code: Block){
+inline infix fun Closure.ensure(code: Block): Any? {
+    try{
+        return this()
+    }
+    finally{
+        code()
+    }
+}
+
+inline infix fun Block.ifCurtailed(code: Block){
     try{
         this()
     }
     catch(ex: Throwable){
         code()
+        throw ex
+    }
+}
+
+inline infix fun Closure.ifCurtailed(code: Block) : Any?{
+    try{
+        return this()
+    }
+    catch(ex: Throwable){
+        code()
+        throw ex
     }
 }
 
@@ -45,8 +66,31 @@ inline fun <reified T: Throwable> Block.on(code: (T) -> Unit){
     }
 }
 
+inline fun <reified T : Throwable> Closure.on(code: (T) -> Unit) : Any?{
+    return try{
+        this()
+    }
+    catch(ex: Throwable){
+        if(ex is T) code(ex)
+        else throw ex
+    }
+}
+
 inline fun <reified T: Throwable> Block.on(code: (T) -> Unit, ensure: Block){
     try{
+        this()
+    }
+    catch(ex: Throwable){
+        if(ex is T) code(ex)
+        else throw ex
+    }
+    finally{
+        ensure()
+    }
+}
+
+inline fun <reified T: Throwable> Closure.on(code: (T) -> Unit, ensure: Block) : Any?{
+    return try{
         this()
     }
     catch(ex: Throwable){
--- src/commonTest/kotlin/ClosuresTest.kt
+++ src/commonTest/kotlin/ClosuresTest.kt
@@ -2,13 +2,16 @@ package com.mysidia.kttalk
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertNull
+import kotlin.test.assertFailsWith
 
 class ClosuresTest {
 
+    private val list = listOf(1, 2, 3, 4, 5)
+    private var message = ""
+    private var message2 = ""
+
     @Test
     fun testWhileTrue(){
-        val list = listOf(1, 2, 3, 4, 5)
         var sum = 0
         var i = 0
         { i < 5 } whileTrue {
@@ -20,13 +23,80 @@ class ClosuresTest {
 
     @Test
     fun testWhileFalse(){
-        val list = listOf(1, 2, 3, 4, 5)
         var sum = 0
-        var i = 5
-        { i > 0 } whileFalse {
+        var i = 4
+        { i < 0 } whileFalse {
             sum += list[i]
             i--
         }
         assertEquals(sum, 15)
+    }
+
+    @Test
+    fun testEnsure(){
+        var message = ""
+        var i = 0
+        { i = list[0] + list[1] } ensure {
+            message = "executed without exception"
+        }
+        assertEquals(message, "executed without exception")
+
+        assertFailsWith<ArithmeticException> {
+            { list[2] / 0 } ensure {
+                message = "executed despite exception"
+            }
+        }
+        assertEquals(message, "executed despite exception")
+    }
+
+    @Test
+    fun testIfCurtailed(){
+        var message = ""
+        var i = 0
+        { i = list[0] + list[1] } ifCurtailed {
+            message = "executed with exception"
+        }
+        assertEquals(message, "")
+
+        assertFailsWith<ArithmeticException> {
+            { list[2] / 0 } ifCurtailed {
+                message = "executed with exception"
+            }
+        }
+        assertEquals(message, "executed with exception")
+    }
+
+    @Test
+    fun testOn(){
+        var message = ""
+        var i = 0
+        { i = list[0] + list[1] }.on<ArithmeticException> { message = "caught exception ${it::class.simpleName}"}
+        assertEquals(message, "");
+
+        { list[2] / 0 }.on<ArithmeticException>{ message = "caught exception ${it::class.simpleName}"}
+        assertEquals(message, "caught exception ArithmeticException")
+    }
+
+    @Test
+    fun testOnEnsureForBlock() {
+        var i = 0
+        { i = list[0] + list[1] }.on<ArithmeticException>({
+            message = "caught exception ${it::class.simpleName}"
+        }, ensure = {
+            message2 = "executed without exception"
+        })
+        assertEquals(message, "")
+        assertEquals(message2, "executed without exception")
+    }
+
+    @Test
+    fun testOnEnsureForClosure(){
+        { list[2] / 0 }.on<ArithmeticException>({
+            message = "caught exception ${it::class.simpleName}"
+        }, ensure = {
+            message2 = "executed without exception"
+        })
+        assertEquals(message, "caught exception ArithmeticException")
+        assertEquals(message2, "executed without exception")
     }
 }
