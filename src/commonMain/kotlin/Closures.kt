1f9d27adf810be0b803b9814534f99ccc4e5229e
Hall of Famer <worldoffame@hotmail.com>
2021-08-29 04:26:35 -0400

    Updated Closures.kt

--- src/commonMain/kotlin/Closures.kt
+++ src/commonMain/kotlin/Closures.kt
@@ -4,6 +4,11 @@ typealias Predicate = () -> Boolean
 typealias Block = () -> Unit
 typealias Closure = () -> Any?
 
+/**
+ * evaluates the supplied code block as long as the receiver predicate evaluates to true.
+ *
+ * @param code block of code to execute while the predicate evaluates to true
+ */
 infix fun Predicate.whileTrue(code: Block) {
     if(this()){
         code()
@@ -11,6 +16,11 @@ infix fun Predicate.whileTrue(code: Block) {
     }
 }
 
+/**
+ * evaluates the supplied code block as long as the receiver predicate evaluates to false.
+ *
+ * @param code block of code to execute while the predicate evaluates to false
+ */
 infix fun Predicate.whileFalse(code: Block){
     if(!this()){
         code()
@@ -18,6 +28,11 @@ infix fun Predicate.whileFalse(code: Block){
     }
 }
 
+/**
+ * evaluates the supplied code block regardless of whether the receiver block executes successfully.
+ *
+ * @param code block of code to execute always
+ */
 inline infix fun Block.ensure(code: Block){
     try{
         this()
@@ -27,6 +42,12 @@ inline infix fun Block.ensure(code: Block){
     }
 }
 
+/**
+ * evaluates the supplied code block regardless of whether the receiver closure executes successfully.
+ *
+ * @param code block of code to execute always
+ * @return Any?
+ */
 inline infix fun Closure.ensure(code: Block): Any? {
     try{
         return this()
@@ -36,6 +57,11 @@ inline infix fun Closure.ensure(code: Block): Any? {
     }
 }
 
+/**
+ * evaluates the supplied code block if the receiver block execution fails with error/exception.
+ *
+ * @param code block of code to execute if error happens
+ */
 inline infix fun Block.ifCurtailed(code: Block){
     try{
         this()
@@ -46,6 +72,12 @@ inline infix fun Block.ifCurtailed(code: Block){
     }
 }
 
+/**
+ * evaluates the supplied code block if the receiver closure execution fails with error/exception.
+ *
+ * @param code block of code to execute if error happens
+ * @return Any?
+ */
 inline infix fun Closure.ifCurtailed(code: Block) : Any?{
     try{
         return this()
@@ -56,6 +88,12 @@ inline infix fun Closure.ifCurtailed(code: Block) : Any?{
     }
 }
 
+/**
+ * evaluates the supplied code block if the receiver block execution throws an exception of type T.
+ *
+ * @param T generic parameter which must implements Throwable interface
+ * @param code block of code to execute if caught Exception of type T
+ */
 inline fun <reified T: Throwable> Block.on(code: (T) -> Unit){
     try{
         this()
@@ -66,6 +104,13 @@ inline fun <reified T: Throwable> Block.on(code: (T) -> Unit){
     }
 }
 
+/**
+ * evaluates the supplied code block if the receiver block execution throws an exception of type T.
+ *
+ * @param T generic parameter which must implements Throwable interface
+ * @param code block of code to execute if caught Exception of type T
+ * @return Any?
+ */
 inline fun <reified T : Throwable> Closure.on(code: (T) -> Unit) : Any?{
     return try{
         this()
@@ -76,6 +121,13 @@ inline fun <reified T : Throwable> Closure.on(code: (T) -> Unit) : Any?{
     }
 }
 
+/**
+ * evaluates the supplied code block if the receiver block execution throws an exception of type T, and always evaluates the second code block.
+ *
+ * @param T generic parameter which must implements Throwable interface
+ * @param code block of code to execute if caught Exception of type T
+ * @param ensure block of code to always execute
+ */
 inline fun <reified T: Throwable> Block.on(code: (T) -> Unit, ensure: Block){
     try{
         this()
@@ -89,6 +141,14 @@ inline fun <reified T: Throwable> Block.on(code: (T) -> Unit, ensure: Block){
     }
 }
 
+/**
+ * evaluates the supplied code block if the receiver closure execution throws an exception of type T, and always evaluates the second code block.
+ *
+ * @param T generic parameter which must implements Throwable interface
+ * @param code block of code to execute if caught Exception of type T
+ * @param ensure block of code to always execute
+ * @return Any?
+ */
 inline fun <reified T: Throwable> Closure.on(code: (T) -> Unit, ensure: Block) : Any?{
     return try{
         this()
